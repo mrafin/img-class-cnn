2b59f742083e7857c0335def3bd0006823b49a27
mrafin <55218099+mrafin@users.noreply.github.com>
2021-12-09 16:25:58 +0700

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,10 @@ def predict_label(img_path):
     q.append(query)
     q = np.array(q, dtype='float') / 255.0
     q_pred = model.predict(q)
-    predicted_bit = int(q_pred)
+    if q_pred < 0.5:
+        predicted_bit = 0
+    else :
+        predicted_bit = 1
     return class_dict[predicted_bit]
 
 @app.route('/', methods=['GET', 'POST'])
